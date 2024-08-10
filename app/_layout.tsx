dbb55788e7dde47c03c45cf9d0e5effad3731549
A-Scode <shouryarajsinghgoud@gmail.com>
2024-08-11 02:30:45 +0530

    app workign

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,9 +27,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={ DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerBackButtonMenuEnabled:false  , headerShown:false}} />
+        <Stack.Screen name="[id]" options={{ headerBackButtonMenuEnabled:false  , headerShown:false}} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
